26f6eef30ca0a987c028076cc3e5a901f31a7199
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-02 18:11:44 +0000

    Corregidas tareas

--- Contenidos/UD2/repaso10.py
+++ Contenidos/UD2/repaso10.py
@@ -2,6 +2,4 @@ for l_num in range (0, 6 + 1):
     for r_num in range (0, 6 + 1):
         if l_num <= r_num:
             print(f'{l_num}|{r_num}', end=" ")
-        else:
-            continue
     print()

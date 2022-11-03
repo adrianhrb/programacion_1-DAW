70f9c6caf5f9381d502b22f777b40ae845f865af
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-03 18:11:46 +0000

    Corregido repaso12.py

--- Contenidos/UD2/repaso12.py
+++ Contenidos/UD2/repaso12.py
@@ -1,9 +1,14 @@
+LINE_SIZE = 5
+INITIAL_CODE = 33
+FINAL_CODE = 127
+lines = ""
 num_char = 0
-for code in range(33, 127 + 1):
+for code in range(INITIAL_CODE, FINAL_CODE + 1):
     ascii_num = chr(code)
     num_char += 1
-    print(f"{code:03}:{ascii_num}", end="  ")
-    #Desplazamiento modular
-    if num_char % 5 == 0:
-        print()
+    lines += f"{code:03}:{ascii_num} "
+    # Desplazamiento modular
+    if num_char % LINE_SIZE == 0:
+        lines += "\n"
 
+print(lines)

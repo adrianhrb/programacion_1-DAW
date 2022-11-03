e0fac48a11a1a96d3ec43a11889f4ef2bc07d6cd
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-03 18:29:37 +0000

    Corregido repaso13.py

--- Contenidos/UD2/repaso13.py
+++ Contenidos/UD2/repaso13.py
@@ -1,15 +1,15 @@
-obj_num = 87
+obj_num = 5
 attempts = 0
-
-while True:
-    guess_number = int(input("Dime un número: "))
-    if guess_number == obj_num:
-        attempts += 1
-        print(f'✅ ¡Enhorabuena! Has encontrado el número en {attempts} intentos')
-        break
-    elif guess_number > obj_num:
-        attempts += 1
-        print('Menor')
-    else:
+guess_number = None
+while guess_number != obj_num:
+    guess_number = float(input("Dime un número: "))
+    if guess_number < obj_num:
         attempts += 1
         print("Mayor")
+    elif guess_number > obj_num:
+        attempts += 1
+        print("Menor")
+
+# Si salimos del bucle los números son iguales
+attempts += 1
+print(f"✅ ¡Enhorabuena! Has encontrado el número en {attempts} intentos")

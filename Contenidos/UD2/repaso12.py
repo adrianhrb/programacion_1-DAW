0b8e42361d2155a4e839b7f1f088229260f6646b
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-03 17:56:24 +0000

    Corregido repaso12.py

--- Contenidos/UD2/repaso12.py
+++ Contenidos/UD2/repaso12.py
@@ -1,7 +1,9 @@
-num_count = 0
-for num in range(33, 127 + 1):
-    ascii_num = chr(num)
-    num_count += 1
-    print(f"{num:03}:{ascii_num}", end="  ")
-    if num_count % 5 == 0:
+num_char = 0
+for code in range(33, 127 + 1):
+    ascii_num = chr(code)
+    num_char += 1
+    print(f"{code:03}:{ascii_num}", end="  ")
+    #Desplazamiento modular
+    if num_char % 5 == 0:
         print()
+

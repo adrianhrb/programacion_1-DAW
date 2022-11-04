8ce491ff0f655b2961738c113db9b932ef27646a
adrianhrb <adrian.herrera.br@gmail.comgit config --global user.email>
2022-11-04 10:33:51 +0000

    Añadido square.py

---
+++ Contenidos/UD2/square_number.py
@@ -0,0 +1,9 @@
+start_number = int(input("Dime un número: "))
+square_number = start_number
+number = start_number
+while True:
+    result = square_number ** 0.5
+    if result 
+        square_number += 1
+    else:
+        print (f'The nearest square number of {start_number} is {square_number}')

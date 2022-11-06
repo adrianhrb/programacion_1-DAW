a8bdc418536e40b305f513eb23f44d01ecd08071
adrianhrb <adrian.herrera.br@gmail.comgit config --global user.email>
2022-11-06 16:53:24 +0000

    Añadidos ejercicios de bucles

---
+++ Contenidos/UD2/char_mistake.py
@@ -0,0 +1,22 @@
+recognized_text = "Hola 50y un amig0 de 1sabel"
+misinterpreted_char5 = "5"
+misinterpreted_char1 = "1"
+misinterpreted_char0 = "0"
+normal_char = " abcdefghijklmnopqrstuvwxyz"
+corrected_text = ""
+
+for char in recognized_text:
+    if char.lower() in normal_char:
+        corrected_text += char
+        continue
+    elif char.lower() in misinterpreted_char0:
+        char_0 = char.replace(char, "o")
+        corrected_text += char_0
+    elif char.lower() in misinterpreted_char1:
+        char_1 = char.replace(char, "i")
+        corrected_text += char_1
+    else:
+        char_5 = char.replace(char, "s")
+        corrected_text += char_5
+
+print(corrected_text)
---
+++ Contenidos/UD2/fake_binary.py
@@ -0,0 +1,15 @@
+digits_string = input("Dime una serie de nº: ")
+
+change_0 = "01234"
+change_1 = "56789"
+resultant_string = ""
+
+for digit in digits_string:
+    if digit in change_0:
+        replace_0 = digit.replace(digit, "0")
+        resultant_string += replace_0
+    else:
+        replace_1 = digit.replace(digit, "1")
+        resultant_string += replace_1
+
+print(resultant_string)

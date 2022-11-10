d8a9b3f1560216291de3660f3fb3efe1de46f78f
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-10 18:23:19 +0000

    Añadido ejercicio de isograma

--- Contenidos/UD3/isogram.py
+++ Contenidos/UD3/isogram.py
@@ -1,7 +1,7 @@
 word = input("Dime algo: ")
 seen_chars = []
 
-for letter in word.lower:
+for letter in word.lower():
     if letter.lower() in seen_chars:
         print("No es un isograma")
         break

463eadca7541a2b113c49b459ec87c4ad700a2df
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-18 16:38:00 +0000

    Corregido ejercicio repaso3

--- Contenidos/UD3/repaso3.py
+++ Contenidos/UD3/repaso3.py
@@ -1,10 +1,16 @@
 entry = [0, 10, [20, 30], 40, 50, [60, 70, 80], [90, 100, 110, 120]]
 flattened_list = []
 
-for number in entry:
+"""for number in entry:
     if type(number) == list:
         for num in number:
             flattened_list.append(num)
+    else:
+        flattened_list.append(number)"""
+
+for number in entry:
+    if type(number) == list:
+        flattened_list.extend(number)
     else:
         flattened_list.append(number)
 

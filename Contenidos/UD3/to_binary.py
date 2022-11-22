c0db9b125ae92d11180ee23ebb40c0e010e28526
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-22 19:42:30 +0000

    Corregido ejercicio binario

--- Contenidos/UD3/to_binary.py
+++ Contenidos/UD3/to_binary.py
@@ -1,13 +1,17 @@
 number = 11
 binary_list = []
 
-while number != 0:
+"""while number != 0:
     bin_number = number % 2
     number -= (number // 2) + (number % 2)
     if bin_number == 0:
         binary_list.append(0)
     else:
-        binary_list.append(1)
+        binary_list.append(1)"""
+while number != 0:
+    bin_number = number % 2
+    number -= (number // 2) + (number % 2)
+    binary_list.append(bin_number)
 
 real_binary = list(reversed(binary_list))
 string = [str(num) for num in real_binary]

df6d984ea152639e41e248430a947b059b421981
adrianhrb <adrian.herrera.br@gmail.com>
2022-11-25 16:50:04 +0000

    Añadido ejercicio consecutivos

---
+++ Contenidos/UD3/non_consecutive.py
@@ -0,0 +1,12 @@
+values = [1, 2, 3, 4, 6, 7, 8]
+target = None
+if len(values) == 0:
+        target = target
+    else:
+        check_number = values[0]
+    for num in values[1:]:
+        if num - 1 == check_number:
+            check_number = num
+        else:
+            target = num
+            break
